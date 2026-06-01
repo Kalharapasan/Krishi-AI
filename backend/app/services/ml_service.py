3cab0aca719f979ae99f0077b78d1f1f76cbb884
Pasan Kalhara <kalharap84@gmail.com>
2026-06-02 00:28:12 +0530

    feat: Implement PlantDiseaseClassifier model architecture in ml_service.py

--- backend/app/services/ml_service.py
+++ backend/app/services/ml_service.py
@@ -17,3 +17,31 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
+
+class PlantDiseaseClassifier(nn.Module):
+    def __init__(self, num_classes=38):
+        super().__init__()
+        self.model = models.efficientnet_b0(weights=None)
+        in_features = self.model.classifier[1].in_features
+        self.model.classifier = nn.Identity()
+
+        self.fc1 = nn.Linear(in_features, 512)
+        self.relu1 = nn.ReLU()
+        self.drop1 = nn.Dropout(0.4)
+
+        self.fc2 = nn.Linear(512, 128)
+        self.relu2 = nn.ReLU()
+        self.drop2 = nn.Dropout(0.3)
+
+        self.fc3 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.drop2(x)
+        x = self.fc3(x)
+        return x
