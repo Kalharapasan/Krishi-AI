ffafd1beb8746002d75612e88159cd59adba8a14
Pasan Kalhara <kalharap84@gmail.com>
2026-06-03 00:22:56 +0530

    feat: Add necessary imports for image processing and API service integration

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'api_service.dart';
+
 
 void main() {
   runApp(const MyApp());
