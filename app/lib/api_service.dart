cc1c8e975aed130fcb0092bb730a31dd741ce6a8
Pasan Kalhara <kalharap84@gmail.com>
2026-06-03 00:36:20 +0530

    fix: Update baseUrl in ApiService to use localhost for API requests

--- app/lib/api_service.dart
+++ app/lib/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   // Default to emulator localhost
-  String baseUrl = 'http://10.0.2.2:8000/api';
+  String baseUrl = 'http://127.0.0.1:8000/api';
 
   void updateBaseUrl(String newUrl) {
     baseUrl = newUrl;
